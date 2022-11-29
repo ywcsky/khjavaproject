5c5dd3a8215934b0ea1fa297edfe52bbc95b6906
서정민 <ywcsky2@gmail.com>
2022-11-29 17:12:22 +0900

    5th commit
    05일차
    1. method, sort, array 정리

--- src/com/kh/day05/array/Exam_SortBubble.java
+++ src/com/kh/day05/array/Exam_SortBubble.java
@@ -3,7 +3,7 @@ package com.kh.day05.array;
 public class Exam_SortBubble {
     public static void main(String[] args) {
         int arr[] = {3, 5, 2, 1, 4};
-
+    int a;
         for(int i = 0; i < arr.length-1; i++){
             for(int j = 0 ; j < (arr.length-1)-i; j++){
                 if(arr[j] > arr[j+1]){
